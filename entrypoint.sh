4edd7c861ae222df9bb645f88cde36bc407bd011
felix1251 <felixabacajen17@gmail.com>
2026-02-06 20:56:03 +0800

    feat: update docker entry poisnt

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 set -e
 
-echo "DB_HOST: $DB_HOST"
-echo "DB_PORT: $DB_PORT"
+echo "DATABASE_URL: $DATABASE_URL"
 
-# Function to wait for PostgreSQL
+# Function to wait for PostgreSQL using DATABASE_URL
 wait_for_postgres() {
-  local host="$1"
-  local port="$2"
-  local max_attempts=20 
+  local database_url="$1"
+  local max_attempts=20
   local attempt=1
 
-  until pg_isready -h "$host" -p "$port"; do
-    if [ $attempt -eq $max_attempts ]; then
+  until pg_isready -d "$database_url"; do
+    if [ "$attempt" -eq "$max_attempts" ]; then
       echo "Postgres is unavailable - exiting after $attempt attempts."
+      exit 1
     fi
     echo "Postgres is unavailable - sleeping ($attempt/$max_attempts)"
-    attempt=$((attempt+1))
+    attempt=$((attempt + 1))
     sleep 5
   done
 
@@ -24,15 +23,15 @@ wait_for_postgres() {
 }
 
 # Wait for PostgreSQL to be available
-wait_for_postgres "${DB_HOST:-postgres}" "${DB_PORT:-3010}"
+wait_for_postgres "$DATABASE_URL"
 
-# Remove any existing server.pid for Rails.
+# Remove any existing server.pid for Rails
 rm -f /app/tmp/pids/server.pid
 
-# Run database migrations
+# Run database setup
 bundle exec rails db:create
 bundle exec rails db:migrate
 bundle exec rails db:seed
 
-# Then exec the container's main process (what's set as CMD in the Dockerfile).
+# Exec the container's main process (CMD)
 exec "$@"
